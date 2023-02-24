69cf565577873af1a36cbb21e38f7c8e69616cc9
dseydahmetova <dseydahmetova@gmail.com>
2023-02-24 02:10:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import NewsDetails from "./Pages/NewsDetails";
+import Nav from "./components/Nav";
+import "./App.css"
+import NewsCard from "./components/NewsCard";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getAllStarships } from "./components/news-api";
+import { MyContext } from './context'
 
 function App() {
+
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const NewsData = async () => {
+      const newsData = await getAllStarships();
+      setNews(newsData.articles);
+    };
+    NewsData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyContext.Provider value={{ news }}>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<NewsCard />} />
+          <Route path="/news/:title" element={<NewsDetails />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </MyContext.Provider>
     </div>
   );
 }
